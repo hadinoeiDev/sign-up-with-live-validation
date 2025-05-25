076f85247ea5a4289a3d3dadbc043ad5f63c943c
Hadi Noei <hadinoei.dev@gmail.com>
2025-05-25 14:45:45 +0330

    feat: work on users list

--- scripts/fetch-get.js
+++ scripts/fetch-get.js
@@ -1,8 +1,30 @@
+const $ = document;
+const bodyElem = $.body;
+const ulELem = $.querySelector('.userListElem');
+
 fetch('https://hadi-noei-sign-up-form-default-rtdb.firebaseio.com/users.json')
     .then(res => {
         console.log(res);
         return res.json();
     })
-    .then(usersData => {
-        console.log(usersData);
-    })
+    .then(data => {
+        
+        let usersData = Object.entries(data);
+
+        usersData.forEach(user => {
+            ulELem.insertAdjacentHTML('beforeend',
+                `<li class="newUserElem">
+                    <div class = "user-data">
+                        <p class="newUserName"><span>${user[1].userName} ${user[1].userLastName}</span></p>
+                        <p class="newUserPassword">password: <span>${user[1].userPassword}</span></p>
+                    </div>
+                    <div class = "buttons">
+                        <button class="editButton">edit</button>
+                        <button class="deleteButton">delete</button>
+                    </div>
+                </li>
+                `
+            );      
+            
+        })
+    });
