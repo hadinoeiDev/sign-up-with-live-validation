0a6bd472c36f9a42b058f8fc641e096f566fb6a2
Hadi Noei <hadinoei.dev@gmail.com>
2025-05-31 12:41:00 +0330

    refactor: separate fetch calls by method (GET, POST, etc.)

---
+++ scripts/fetch-DELETE.js
@@ -0,0 +1,23 @@
+function deleteBtn(event) {
+
+    // remove from dom
+
+    let getParentElm = event.target.parentElement.parentElement;
+    getParentElm.remove();
+
+    // remove from fireBase
+
+    const userId = event.target.dataset.id;
+
+    fetch(`https://hadi-noei-sign-up-form-default-rtdb.firebaseio.com/users/${userId}.json`, {
+        method: 'DELETE'
+    })
+        .then(res => {
+            return res.json();    
+        })
+        .then(data => {
+            console.log(data);
+        })
+
+    
+}  
--- scripts/fetch-get.js
+++ scripts/fetch-get.js
@@ -32,28 +32,3 @@ fetch('https://hadi-noei-sign-up-form-default-rtdb.firebaseio.com/users.json')
             );      
         });
     });
-
-
-function deleteBtn(event) {
-
-    // remove from dom
-
-    let getParentElm = event.target.parentElement.parentElement;
-    getParentElm.remove();
-
-    // remove from fireBase
-
-    const userId = event.target.dataset.id;
-
-    fetch(`https://hadi-noei-sign-up-form-default-rtdb.firebaseio.com/users/${userId}.json`, {
-        method: 'DELETE'
-    })
-        .then(res => {
-            return res.json();    
-        })
-        .then(data => {
-            console.log(data);
-        })
-
-    
-}  
