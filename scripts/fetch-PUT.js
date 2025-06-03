8904a17da8c02c5fed0b6aa8bad1097415c1acbd
Hadi Noei <hadinoei.dev@gmail.com>
2025-06-03 11:05:15 +0330

    feat: add edit feature (PUT method) (part 4)

--- scripts/fetch-PUT.js
+++ scripts/fetch-PUT.js
@@ -4,24 +4,130 @@ const lastNmeEdit = $.querySelector('.lastNmeEdit');
 const emailEdit = $.querySelector('.emailEdit');
 const submitEdit = $.querySelector('.submitEdit');
 
+const firstNameEditCheck = $.querySelector('#firstNameEditCheck');
+const lastNameEditCheck = $.querySelector('#lastNameEditCheck');
+const emailEditCheck = $.querySelector('#emailEditCheck');
+
+let isFirstNameEditValid = false;
+let isLastNameEditValid = false;
+let isEmailEditValid = false;
+
 let userIdToEdit = null;
+
+function firstNameEditValidation() {
+     let val = firstNmeEdit.value;
+
+    const hasOnlyEnglishChars = /^[A-Za-z]+$/.test(val);
+
+    if(val.trim() === '') {
+        return isFirstNameEditValid = false;
+    }
+    
+    if(hasOnlyEnglishChars) {
+        isFirstNameEditValid = true;
+        firstNameEditCheck.style.display = 'none';
+
+    } else {
+        isFirstNameEditValid = false;
+        firstNameEditCheck.style.display = 'flex';
+    }
+}
+
+function lastNameEditValidation() {
+     let val = lastNmeEdit.value;
+
+    const hasOnlyEnglishChars = /^[A-Za-z]+$/.test(val);
+
+    if(val.trim() === '') {
+        return isLastNameEditValid = false;
+    }
+    
+    if(hasOnlyEnglishChars) {
+        isLastNameEditValid = true;
+        lastNameEditCheck.style.display = 'none';
+
+    } else {
+        isLastNameEditValid = false;
+        lastNameEditCheck.style.display = 'flex';
+    }
+}
+
+
+function emailEditValidation() {
+    const val = emailEdit.value;  // اگر userPassword یعنی input ایمیله اوکیه
+
+    const emailValidRegex = /^[a-zA-Z0-9._%+-]+@(gmail|yahoo)\.com$/.test(val);
+
+    const targetEmail = emailEdit.value; 
+
+     
+    if (val.trim() === '') {
+        return isEmailEditValid = false;
+    }
+    
+    fetch('https://hadi-noei-sign-up-form-default-rtdb.firebaseio.com/users.json')
+        .then(res => {
+            console.log(res);
+            return res.json();
+        })
+        .then(data =>{
+            let objData = Object.entries(data);
+
+            let someUserEmail = objData.some(userData => {
+                return userData[1].userEmail === targetEmail;
+            });
+
+            if(someUserEmail) {
+                console.log('there is a same email');
+                isEmailEditValid = false;
+                emailEditCheck.style.color = 'red';
+                emailEditCheck.textContent = "Enter a valid and unique email address.";
+            }
+        })
+        .catch(err => {
+            console.error(`data base is empty : ${err}`);
+        })
+        
+
+    if(val.trim() === '') {
+        isEmailEditValid = false;
+        emailEditCheck.style.color = 'red';
+        return;
+    }
+
+    emailEditCheck.style.display = "flex";
+    
+    if (emailValidRegex) {
+        emailEditCheck.textContent = 'Valid email';
+        emailEditCheck.style.color = '#00c500';
+        isEmailEditValid = true;
+
+    } else {
+        emailEditCheck.textContent = "Enter a valid and unique email address!";
+        emailEditCheck.style.color = 'red';
+        isEmailEditValid = false;
+
+    }
+};
+
+
 
 function editBtn(event) {
     editContainer.style.display = 'flex';
     userIdToEdit = event.target.dataset.id;
-
+    
     fetch(`https://hadi-noei-sign-up-form-default-rtdb.firebaseio.com/users/${userIdToEdit}.json`)
-        .then(res => res.json())
-        .then(userData => {
-            // ذخیره پسورد قبلی
-            oldPassword = userData.userPassword;
-            // پر کردن فرم با اطلاعات کاربر
-            firstNmeEdit.value = userData.userFirstName;
-            lastNmeEdit.value = userData.userLastName;
-            emailEdit.value = userData.userEmail;
-        });
-
-
+    .then(res => res.json())
+    .then(userData => {
+        // ذخیره پسورد قبلی
+        oldPassword = userData.userPassword;
+        // پر کردن فرم با اطلاعات کاربر
+        firstNmeEdit.value = userData.userFirstName;
+        lastNmeEdit.value = userData.userLastName;
+        emailEdit.value = userData.userEmail;
+    });
+    
+    
 }
 
 function submitEditBtn(event) {
@@ -33,23 +139,26 @@ function submitEditBtn(event) {
         userPassword: oldPassword,
         userEmail: emailEdit.value,
     }
-
+    
     fetch(`https://hadi-noei-sign-up-form-default-rtdb.firebaseio.com/users/${userIdToEdit}.json`, {
         method: 'PUT',
         headers: {
-          "Content-Type": "application/json",
+            "Content-Type": "application/json",
         },
         body: JSON.stringify(editedData)
     })
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            console.log(data);
-
-        })
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        console.log(data);
+        
+    })
     
     editContainer.style.display = 'none';
 }
 
+firstNmeEdit.addEventListener('input', firstNameEditValidation);
+lastNmeEdit.addEventListener('input', lastNameEditValidation);
+emailEdit.addEventListener('input', emailEditValidation);
 submitEdit.addEventListener('click',submitEditBtn);
